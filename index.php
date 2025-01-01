96171b6511d0b8ae5bd1f8a426faeaa2d0daac9d
Ashok <negiashok132@gmail.com>
2025-01-02 04:51:57 +0530

    5 step -> in this case i create a modal box where i can create input field , this modal open when we click the edit button when we want to update user data and we can easily update a user data , this is Complete CRUD application using PHP,AJA and Jquery

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
             background-color: white;
             text-align: center;
         }
+        #formfields input{
+            border-radius: 20px;
+        }
         #header{
             background-color: yellow;
         }
@@ -27,9 +30,69 @@
             background-color: blue;
             color: white;
         }
+        #modal{
+		 background: rgba(0, 0, 0, 0.7);
+		 position: fixed;
+		 left: 0;
+		 top: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 100;
+        display: none;
+		
+		}
+		#modal-form{
+			background: #fff;
+			width:40%;
+			position: relative;
+			top:15%;
+			left: calc(60% - 20%);
+			padding: 15px;
+			border-radius: 4px;
+		}   
+		#modal-form h2{
+  			margin: 0 0 15px;
+  			padding-bottom: 10px ;
+  			border-bottom: 1px solid black;
+		}
+        #close-btn{
+			background: red;
+			color: white;
+			width: 30px;
+			height: 30px;
+			line-height: 30px;
+			text-align: center;
+			border-radius: 50px;
+			position: absolute;
+			top: -15px;
+			right: -15px;
+			cursor: pointer;
+		}
+        #modal-form table input{
+            border-radius: 20px;
+            margin:6px;
+        }
+        #update-btn{
+            background-color: green;
+            color: white;
+        }
+       
+      
     </style>
 </head>
 <body>
+   
+<div id="modal">
+			<div id="modal-form">
+				<h2>Edit From</h2>
+				<table cellpadding="10px" width="100%">
+                    
+				</table>
+				<div id="close-btn" style="margin-top:20px" >X</div>
+			</div>
+		</div>
+
+
     <table id="main" border="0px" cellpspacing="0px">
         <tr>
             <td id="header">
@@ -37,10 +100,11 @@
         </tr>
 
         <tr>
-            <td id="tableform"><form id="formfields">
-                First Name : <input type="text" name="fname" id="fname">
-                Last Name :<input type="text" name="lname" id="lname">
-                <input type="submit" id="btn" value="Save"> 
+            <td id="tableform">
+                 <form id="formfields">
+                  First Name : <input type="text" name="fname" id="fname">
+                  Last Name :<input type="text" name="lname" id="lname">
+                  <input type="submit" id="btn" style="background-color:green;color:white" value="Save"> 
                 </form>
             </td>
         </tr>
@@ -62,12 +126,9 @@
                     type : 'GET',
                     success : function(data){
                     $("#table-data").html(data);
-                    },
-                    complete :function(xhr,status){
-                        console.log(xhr.status);
-                        console.log(status);
                     }
-                }); // ajax function
+                    
+                }); // ajax functions
             } // loadtable function
             loadtable();  // call load table function
 
@@ -113,7 +174,7 @@
                     data : {id : userid},
                     success  : function(data){
                         if(data==1){
-                        $(element).closest("tr").fadeOut();
+                        $(element).closest("tr").fadeOut(1000);
                         }else{
                             alert("failed");
                         }
@@ -126,6 +187,61 @@
                 });
             });
 
+        
+   // 4 step ->  this is for edit the user data 
+                $(document).on("click",".edit-btn",function(e){
+                        $('#modal').slideDown(2000);
+                        var user_id = $(this).data("eid");
+            
+                      // fetch user data from database for showing the data into input filed so user can easily modify the details
+                        $.ajax({
+                            url : "edit-data.php",
+                            type : "GET",
+                            data : {id : user_id},
+                            success :function(data){
+                                $("#modal-form table").html(data);
+                            },
+                            complete : function(xhr,status){
+                                console.log(status);
+                                console.log(xhr.status);
+                            }
+                        });
+
+                });     
+                
+  // step 5 -> this is for close the modal box               
+                $("#close-btn").click(function(e){
+                    $("#modal").slideUp(2000);
+                });
+
+  // step 6 -> this code is update butoon after changing/ modified the user data when we click the update button
+             $(document).on("click","#update-btn",function(e){
+                    e.preventDefault();
+                var user_id = $(this).data('uid');
+                var fname = $("#first_name").val();
+                var lname = $("#last_name").val();
+                
+                $.ajax({
+                    url : "update-data.php",
+                    type : "POST",
+                    data : { id : user_id ,first_name:fname , last_name:lname},
+                    success  : function(data){
+                        if(data==1){
+                            $("#modal").slideUp(1000);
+                            loadtable();
+                        }else{
+                            alert("failed");
+                            $("#modal").slideUp();
+                        }
+                    },
+                    complete : function(xhr,status){
+                        console.log(xhr.status);
+                        console.log(xhr.statusText);
+                    }
+                });
+
+             });
+    
 
         }); // ready function close
     </script>
