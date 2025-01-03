98616ad2648e31fa390d875eba50ff29295446dd
Ashok <negiashok132@gmail.com>
2025-01-03 22:44:37 +0530

    update-data.php

--- update-data.php
+++ update-data.php
@@ -1,10 +1,8 @@
 <?php
 // this is update the user data when update button trigger
-
 $user_id  = $_POST['id'];
 $fname = $_POST['first_name'];
 $lname = $_POST['last_name'];
-
 include('conn.php');
 $sql = "UPDATE student SET first_name = '{$fname}' , last_name = '{$lname}' WHERE id = {$user_id} ";
 if(mysqli_query($conn,$sql)){
